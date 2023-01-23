1c626f1bf9e6aa911bec98fe6bd07cddadfa00d0
balazsNorbert <balazs.norbert@student.ms.sapientia.ro>
2023-01-23 22:54:39 +0200

    Fixing bugs. Get request for composePage.Starting Compose page.Good route path in the header.

--- app.js
+++ app.js
@@ -20,13 +20,16 @@ app.get("/",function(req,res){
 });
 
 app.get("/about",function(req,res){
-  res.render("home",{aboutContent:aboutContent});
+  res.render("about",{aboutContent:aboutContent});
 });
 
 app.get("/contact",function(req,res){
-  res.render("home",{contactContent:contactContent});
+  res.render("contact",{contactContent:contactContent});
 });
 
+app.get("/compose",function(req,res){
+  res.render("compose");
+});
 
 
 
