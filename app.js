7ef79a21ca069ff40e51782a0256c36b02221181
balazsNorbert <balazs.norbert@student.ms.sapientia.ro>
2023-01-24 21:35:26 +0200

    Render the correct blog post

--- app.js
+++ app.js
@@ -49,29 +49,22 @@ app.post("/compose",function(req,res){
     title: req.body.postTitle,
     content: req.body.postBody
   });
-  post.save();
-  res.redirect("/");
-})
-
-app.get("/posts/:postName",function(req,res){
-  const requestedTitle = _.lowerCase(req.params.postName);
-  posts.forEach(function(post){
-    const storedTitle = _.lowerCase(post.title);
-    if(storedTitle === requestedTitle){
-      res.render("post",{
-        title: post.title,
-        content: post.content
-      });
+  post.save(function(err){
+    if(!err){
+      res.redirect("/");
     }
   });
 });
 
-
-
-
-
-
-
+app.get("/posts/:postId",function(req,res){
+  const requestedPostId = req.params.postId;
+  Post.findOne({_id: requestedPostId}, function(err, post){
+      res.render("post", {
+      title: post.title,
+      content: post.content
+  });
+  });
+});
 
 
 
